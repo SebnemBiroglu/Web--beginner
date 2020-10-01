ac170fc06de24c40199ef425f807d24efada9aee
SebnemBiroglu <72161672+SebnemBiroglu@users.noreply.github.com>
2020-10-01 14:32:50 +0300

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,45 @@
+/*alert('Hoş Geldiniz');*/
+!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="4.css">
+	<script src="4.js"></script>
+</head>
+<body>
+<div class="container">
+	<header class="header">
+
+		<a href="/">goShopping</a>
+	</header>
+
+	<nav class="menu">
+		<a href="">Home</a> &nbsp; &nbsp; &nbsp;
+		<a href="about.html">About Us</a> &nbsp; &nbsp; &nbsp;
+		<a href="contact.html">Contact</a>
+	</nav>
+
+	<aside>
+		
+		<h1>Side bar</h1>
+
+	</aside>
+
+	<article>
+		<article class="main">
+
+
+			<p> In the letter, shared with reporters on July 30, Ms. Jean and Mr. Buchanan asked for “a constructive, working dialogue” about how to best support the country’s Black designers ahead of Milan Fashion Week in September.</p>
+
+
+	</article>
+
+	<footer>
+		&copy; 2020 goShopping &nbsp; Design by Şebnem-Selin <a href="http://wwww.edwindiaz.com">www.sebnemselin@com</a>
+	</footer>
+
+</div>
+
+</body>
+</html>
